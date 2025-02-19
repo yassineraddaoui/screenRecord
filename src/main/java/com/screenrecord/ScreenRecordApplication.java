d7bfde441ad0f759f207bc004c680575bb86b026
yassineraddaoui <yassineraddaouicontact@gmail.com>
2025-02-19 12:16:18 +0100

    Initial commit

---
+++ src/main/java/com/screenrecord/ScreenRecordApplication.java
@@ -0,0 +1,13 @@
+package com.screenrecord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenRecordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScreenRecordApplication.class, args);
+    }
+
+}
